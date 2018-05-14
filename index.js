b1beb9e43e6e257b8d0b6967b7bf1917e230a521
ansonmela <anson.melakayil@yahoo.com>
2018-05-14 16:10:16 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const table_data = [ { first_name : 'Rose',
+                     last_name  : 'Tyler',
+                     home       : 'Earth' },
+                   { first_name : 'Zoe',
+                     last_name  : 'Heriot',
+                     home       : 'Space Station W3'},
+                   { first_name : 'Jo',
+                     last_name  : 'Grant',
+                     home       : 'Earth'},
+                   { first_name : 'Leela',
+                     last_name  : null,
+                     home       : 'Unspecified'},
+                   { first_name : 'Romana',
+                     last_name  : null,
+                     home       : 'Gallifrey'},
+                   { first_name : 'Clara',
+                     last_name  : 'Oswald',
+                     home       : 'Earth'},
+                   { first_name : 'Adric',
+                     last_name  : null,
+                     home       : 'Alzarius'},
+                   { first_name : 'Susan',
+                     last_name  : 'Foreman',
+                     home       : 'Gallifrey'} ];
+
+$(document).ready(function() {
+	let data = '';
+
+	$.each(table_data, function(i, table_data) {
+		data += ('<tr><td>'+table_data.first_name+'</td><td>'+table_data.last_name+'</td><td>'+table_data.home+'</td></tr>');
+	});
+	$('.table_data tbody').html(data);
+});
+
+
+
